d5a28afd5532fffe1765d10796d5c5434e0ff6ea
Onran <kotshel2009@gmail.com>
2026-03-17 01:17:27 +0900

    added dot and cross functions to avec3

--- src/util/array_vec3.js
+++ src/util/array_vec3.js
@@ -43,3 +43,15 @@ export function is_zero(a) {
         a[1] === 0 &&
         a[2] === 0;
 }
+
+export function dot(a, b) {
+    return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+}
+
+export function cross(a, b) {
+    return [
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]
+    ]
+}
