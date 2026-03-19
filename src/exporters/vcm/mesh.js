7c705979a51fea831667be52249245eb97200bc0
Onran <kotshel2009@gmail.com>
2026-03-19 16:33:40 +0900

    fixed quads triangulation and added mesh rotation

--- src/exporters/vcm/mesh.js
+++ src/exporters/vcm/mesh.js
@@ -5,11 +5,11 @@ function getTriangles(mesh, face, faceKey) {
     const v = face.vertices
 
     switch(v.length) {
-        case 3: return v
+        case 3: return [v]
 
         case 4: return [
             [v[0], v[1], v[2]],
-            [v[3], v[2], v[1]]
+            [v[0], v[2], v[3]]
         ]
 
         default:
@@ -25,9 +25,20 @@ export default function exportMesh(mesh, builder, parentInfo, indent) {
         const face = mesh.faces[faceKey]
 
         for(const triangle of getTriangles(mesh, face, faceKey)) {
-            const a = avec3.add(mesh.vertices[triangle[0]], relativeOrigin)
-            const b = avec3.add(mesh.vertices[triangle[1]], relativeOrigin)
-            const c = avec3.add(mesh.vertices[triangle[2]], relativeOrigin)
+            const a = avec3.add(
+                avec3.rotate(mesh.vertices[triangle[0]], mesh.rotation),
+                relativeOrigin
+            )
+
+            const b = avec3.add(
+                avec3.rotate(mesh.vertices[triangle[1]], mesh.rotation),
+                relativeOrigin
+            )
+
+            const c = avec3.add(
+                avec3.rotate(mesh.vertices[triangle[2]], mesh.rotation),
+                relativeOrigin
+            )
 
             let texture = texture_util.findTexture(face.texture)
 
