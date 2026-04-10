a44b46abb515f38e03045c9ce058430e42b078c2
Onran <kotshel2009@gmail.com>
2026-04-10 19:42:56 +0900

    Commit Message

--- src/util/data_buffer.js
+++ src/util/data_buffer.js
@@ -1,51 +1,111 @@
 const textEncoder = new TextEncoder()
 
 export default class DataBuffer {
-    #data
+    #buffer
+    #view
+    #offset
 
-    constructor() {
-        this.#data = [ ]
+    constructor(initialSize = 64) {
+        this.#offset = 0
+        this.#buffer = new ArrayBuffer(initialSize)
+        this.#view = new DataView(this.#buffer)
     }
 
-    #putDirect(arr) {
-        this.#data.push(...arr)
+    #ensureCapacity(size) {
+        if (this.#offset + size <= this.#buffer.byteLength) return
+
+        let newSize = this.#buffer.byteLength * 2
+        while (newSize < this.#offset + size) {
+            newSize *= 2
+        }
+
+        const newBuffer = new ArrayBuffer(newSize)
+        new Uint8Array(newBuffer).set(new Uint8Array(this.#buffer))
+
+        this.#buffer = newBuffer
+        this.#view = new DataView(this.#buffer)
     }
 
-    #putBuf(buf) {
-        this.#data.push(
-            ...new Uint8Array(buf)
-        )
+    putInt8(val) {
+        this.#ensureCapacity(1)
+        this.#view.setInt8(this.#offset, val)
+        this.#offset += 1
     }
 
-    #put(ctor, val) {
-        this.#putBuf(ctor(val).buffer)
+    putUint8(val) {
+        this.#ensureCapacity(1)
+        this.#view.setUint8(this.#offset, val)
+        this.#offset += 1
     }
 
-    putFloat32(val) { this.#put(Float32Array, val) }
+    putInt16(val) {
+        this.#ensureCapacity(2)
+        this.#view.setInt16(this.#offset, val, true)
+        this.#offset += 2
+    }
 
-    putFloat64(val) { this.#put(Float64Array, val) }
+    putUint16(val) {
+        this.#ensureCapacity(2)
+        this.#view.setUint16(this.#offset, val, true)
+        this.#offset += 2
+    }
 
-    putInt8(val) { this.#put(Int8Array, val) }
+    putInt32(val) {
+        this.#ensureCapacity(4)
+        this.#view.setInt32(this.#offset, val, true)
+        this.#offset += 4
+    }
 
-    putInt16(val) { this.#put(Int16Array, val) }
+    putUint32(val) {
+        this.#ensureCapacity(4)
+        this.#view.setUint32(this.#offset, val, true)
+        this.#offset += 4
+    }
 
-    putInt32(val) { this.#put(Int32Array, val) }
+    putFloat32(val) {
+        this.#ensureCapacity(4)
+        this.#view.setFloat32(this.#offset, val, true)
+        this.#offset += 4
+    }
 
-    putInt64(val) { this.#put(BigInt64Array, val) }
+    putFloat64(val) {
+        this.#ensureCapacity(8)
+        this.#view.setFloat64(this.#offset, val, true)
+        this.#offset += 8
+    }
 
-    putUint8(val) { this.#putBuf(val) }
+    putInt64(val) {
+        this.#ensureCapacity(8)
+        this.#view.setBigInt64(this.#offset, BigInt(val), true)
+        this.#offset += 8
+    }
 
-    putUint16(val) { this.#put(Uint16Array, val) }
+    putUint64(val) {
+        this.#ensureCapacity(8)
+        this.#view.setBigUint64(this.#offset, BigInt(val), true)
+        this.#offset += 8
+    }
 
-    putUint32(val) { this.#put(Uint32Array, val) }
+    putBytes(arr) {
+        this.#ensureCapacity(arr.length)
+        new Uint8Array(this.#buffer, this.#offset, arr.length).set(arr)
+        this.#offset += arr.length
+    }
 
-    putUint64(val) { this.#put(BigUint64Array, val) }
-
-    putUtf(val) {
-        this.#putDirect(textEncoder.encode(val))
+    putUtf(str) {
+        const encoded = textEncoder.encode(str)
+        this.putBytes(encoded)
     }
 
     getArrayBuffer() {
-        return new Uint8Array(this.#data).buffer
+        return this.#buffer.slice(0, this.#offset)
+    }
+
+    getUint8Array() {
+        return new Uint8Array(this.#buffer, 0, this.#offset)
+    }
+
+    reset() {
+        this.#offset = 0
     }
 }
